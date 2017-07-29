06fd929e7180ca943c6bdc8cc24bf8fca5607037
Joseph Leon <joseph.leon.9@gmail.com>
2017-07-29 14:17:43 -0400

    Write db schema for wines table, change PG to different version.

--- models/database.js
+++ models/database.js
@@ -1,30 +1,18 @@
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/wine_whisper_api';
 
-const wines_schema = '
-      id SERIAL PRIMARY KEY,
-      recommended BOOLEAN,
-      name VARCHAR(40) not null,
-      price smallint,
-      year smallint,
-      rating smallint,
-      company VARCHAR(40) not null,
-      description text
-      ';
+const wines_schema = 'CREATE TABLE wines(id SERIAL PRIMARY KEY, \
+      recommended BOOLEAN, \
+      name VARCHAR(40) not null, \
+      price smallint, \
+      year smallint, \
+      rating smallint, \
+      company VARCHAR(40) not null, \
+      description text)';
+
+// throw new Error("Something went badly wrong!");
 
 const client = new pg.Client(connectionString);
 client.connect();
-const query = client.query(
-  'CREATE TABLE wines
-    (
-      id SERIAL PRIMARY KEY,
-      recommended BOOLEAN,
-      name VARCHAR(40) not null,
-      price smallint,
-      year smallint,
-      rating smallint,
-      company VARCHAR(40) not null,
-      description text
-    )
-  ');
+const query = client.query(wines_schema);
 query.on('end', () => { client.end(); });
